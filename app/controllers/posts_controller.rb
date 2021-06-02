faf12edcc830d58ba253527e75f990b95aeefc35
Kenny Ortega <kennyaoc@gmail.com>
2021-06-02 12:52:47 -0400

    View and create post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def new
+
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
