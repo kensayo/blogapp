cfb7750f6203b015544e9a4f986c94d4ce9bb3e7
Kenny Ortega <kennyaoc@gmail.com>
2021-06-04 16:12:19 -0400

    Created comments section

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  has_many :comments
   validates :title, presence: true, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 
 Rails.application.routes.draw do
   root 'posts#index', as: 'home'
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 end
